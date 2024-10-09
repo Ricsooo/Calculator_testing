0692e5a7c47d4e507327ad63ba74d8cb6cbb12f6
Nanasi Richard <nanasi.richard@diak.szbi-pg.hu>
2024-10-09 09:45:01 +0200

    .\test_Calc.py

--- test_Calc.py
+++ test_Calc.py
@@ -15,3 +15,16 @@ def test_substract():
     assert substract(-10,4) == -14
     assert substract(-3,-4) == 1
     assert substract(0,0) == 0
+
+def test_multiply():
+    assert add(1, 2) == 2
+    assert add(5,5) == 25
+    assert add(-1,1) == -1
+    assert add(-1,-1) == 1
+    assert add(0,0) == 0
+    
+def test_divide():
+    assert add(2, 2) == 1
+    assert add(5,5) == 1
+    assert add(31,1) == 31
+    assert add(-1,-1) == 1
